faad30e6909961bd6ec8e3af83162526910bd402
marcelloromani <marcello.romani@smartpension.co.uk>
2022-03-14 13:29:21 +0000

    add code

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # pi_approx
+
 Approximate PI value using a MonteCarlo method
+
+Reference:
+
+https://twitter.com/SteffPy/status/1503293521083645952
---
+++ main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from random import random
+
+
+def approx_pi(cycle_count: int) -> float:
+    x = [(random(), random()) for i in range(cycle_count)]
+    z = [y[0] ** 2 + y[1] ** 2 < 1 for y in x]
+    pi = 4.0 * sum(z) / len(z)
+    return pi
+
+
+def main():
+    tests = [
+        100,
+        1000,
+        10000,
+        100000,
+    ]
+    for t in tests:
+        pi = approx_pi(t)
+        print(f"{t}  ==>  {pi}")
+
+
+if __name__ == "__main__":
+    main()
